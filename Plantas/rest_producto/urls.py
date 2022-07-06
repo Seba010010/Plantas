3eb5ac10a6fd23ab78873825ec56bf55f64a0aed
Seba010010 <s.santibanezbravo@gmail.com>
2022-07-05 20:20:22 -0400

    creacion GET, POST, DELETE

--- Plantas/rest_producto/urls.py
+++ Plantas/rest_producto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_producto.views import lista_productos
+from rest_producto.views import lista_productos, detalle_productos
 
 urlpatterns = [
-    path('lista_productos', lista_productos, name="lista_productos")
+    path('lista_productos', lista_productos, name="lista_productos"),
+    path('detalle_productos/<id>', detalle_productos, name= "detalle_productos"),
 ]
--- Plantas/rest_producto/views.py
+++ Plantas/rest_producto/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -8,6 +9,8 @@ from core.models import producto
 from .serializers import ProductoSerializer
 #from Plantas.rest_producto import serializers
 
+
+#CREANDO SERVICIOS 
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def lista_productos(request):
@@ -26,3 +29,29 @@ def lista_productos(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#CREANDO SERVICIOS REST PUT, DELETE
+@api_view(['GET', 'POST', 'DELETE'])
+def detalle_productos(request, id):
+
+    "GET, UPDTAE o DELETE de un producto"
+
+    try:
+        productos = producto.objects.get(id_producto = id)
+    except producto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProductoSerializer(productos)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProductoSerializer(productos, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        productos.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
